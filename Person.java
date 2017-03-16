dd587590f3ed0b8d63d1929eb39b5d15542b826f
Eryk <erykurbas2@gmail.com>
2017-03-16 16:07:26 +0100

    Adding second constructor

--- Person.java
+++ Person.java
@@ -5,10 +5,18 @@ public class Person
         private float weight;
         private String name;
 
+        // Constructor1
         public Person(String name)
         {       
                 System.out.println("Person name is: " + name);
                 this.name = name;
         }
 
+        // Constructor2
+        public Person(String name, int age)
+        {
+                System.out.println("Person name is: " + name + " and age: " + age);
+                this.name = name;
+                this.age = age;
+        }        
 }
