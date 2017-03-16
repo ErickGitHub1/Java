62b0628cc5e9f588d9578a59c3a670eec8c0fa03
Eryk <erykurbas2@gmail.com>
2017-03-16 12:29:33 +0100

    Class Dog

---
+++ Dog.java
@@ -0,0 +1,23 @@
+public class Dog
+{
+        //pola
+        int age;
+        String breed;
+        String color;
+
+        //metody
+        void barking()
+        {
+                //implmenentacja metody
+        }
+
+        void hungry()
+        {
+                //implementacja metody
+        }
+
+        void sleeping()
+        {
+                //implementacja metody
+        }
+}
