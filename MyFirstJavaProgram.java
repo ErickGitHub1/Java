8e741aa77af390711d1c508ac85ea1a96507ecf7
Eryk <erykurbas2@gmail.com>
2017-03-16 12:45:05 +0100

    Creation myDog object

--- MyFirstJavaProgram.java
+++ MyFirstJavaProgram.java
@@ -2,6 +2,6 @@ public class MyFirstJavaProgram
 {
         public static void main(String []args)
         {
-                System.out.println("Dziala ... ");
+                Dog myDog = new Dog("Misiek");
         }
 }
