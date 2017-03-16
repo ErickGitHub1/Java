247a912912598c1459c4916348e985326ac94400
Eryk <erykurbas2@gmail.com>
2017-03-16 15:48:14 +0100

    Creation new class Person

---
+++ Person.java
@@ -0,0 +1,14 @@
+public class Person
+{
+        private int age;
+        private float height;
+        private float weight;
+        private String name;
+
+        public Person(String name)
+        {       
+                System.out.println("Person name is: " + name);
+                this.name = name;
+        }
+
+}
