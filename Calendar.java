f8e98c27d5131112589f3e91692c94c7648c1861
Eryk <erykurbas2@gmail.com>
2017-03-22 11:22:34 +0100

    Adding constructor to Calendar class

--- Calendar.java
+++ Calendar.java
@@ -1,4 +1,11 @@
+import java.util.Date;
+
 public class Calendar
 {
-	
+		// Constructor
+		public Calendar()
+		{
+				Date date = new Date();
+				System.out.println("Dzisiejsza data to: " + date);
+		}
 }
